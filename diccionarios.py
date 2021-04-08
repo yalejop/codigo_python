6916dfa0592f78c70535e86ae1773ee0b8b80077
Yalejop <alejo.9211@hottmail.com>
2021-04-07 21:41:16 -0500

    Learn about dicts in python

---
+++ diccionarios.py
@@ -0,0 +1,17 @@
+def run():
+    poblacion_paises = {
+      'Argentina': 44938712,
+      'Brasil': 210147125,
+      'Colombia': 50372424,
+    }
+
+    #print(poblacion_paises['Argentina'])
+    #for pais in poblacion_paises.keys():
+        #print(pais)
+    #for pais in poblacion_paises.values():
+        #print(pais)
+    for pais, poblacion in poblacion_paises.items():
+        print(pais + ' Tiene ' + str(poblacion) + ' Habitantes')
+
+if __name__ == '__main__':
+    run()
